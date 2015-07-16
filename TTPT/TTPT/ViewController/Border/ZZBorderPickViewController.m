9d93f9305d421517376e64fabeed583fa302e9da
quankevin <bu88@bu88deMBP.lan>
2015-07-16 15:05:03 +0800

    边框。9

--- TTPT/TTPT/ViewController/Border/ZZBorderPickViewController.m
+++ TTPT/TTPT/ViewController/Border/ZZBorderPickViewController.m
@@ -61,10 +61,10 @@
     _borderToolsView = [[BorderToolsView alloc ]initWithFrame:CGRectMake(0, self.view.bounds.size.height - 50 - STICKERITEM_HEIGHT, self.view.bounds.size.width,STICKERITEM_HEIGHT)];
     [_borderToolsView setSelectBlock:^(UIImage *image){
         // 左端盖宽度
-        CGFloat top = 340; // 顶端盖高度
-        CGFloat bottom = 340; // 底端盖高度
-        CGFloat left = 340; // 左端盖宽度
-        CGFloat right = 340; // 右端盖宽度
+        CGFloat top = image.size.height/2-1; // 顶端盖高度
+        CGFloat bottom = image.size.height/2-1; // 底端盖高度
+        CGFloat left = image.size.width/2-1; // 左端盖宽度
+        CGFloat right = image.size.width/2-1; // 右端盖宽度
         UIEdgeInsets insets = UIEdgeInsetsMake(top, left, bottom, right);
         image = [image resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch];
         [frameView setImage:image];
